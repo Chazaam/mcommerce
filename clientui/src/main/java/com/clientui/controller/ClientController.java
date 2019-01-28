584c26358a9c0357117102fa0210254a0d47a212
chazaam <samuel.lepajolec@gmail.com>
2019-01-28 17:55:31 +0100

    step 3 : Use Case 5, 6 & 7

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -2,16 +2,21 @@ package com.clientui.controller;
 
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.clientui.beans.CommandeBean;
+import com.clientui.beans.PaiementBean;
 import com.clientui.beans.ProductBean;
 import com.clientui.proxies.MicroserviceCommandeProxy;
+import com.clientui.proxies.MicroservicePaiementProxy;
 import com.clientui.proxies.MicroserviceProduitsProxy;
 
 @Controller
@@ -22,6 +27,9 @@ public class ClientController {
 	
 	@Autowired
 	private MicroserviceCommandeProxy CommandesProxy;
+	
+	@Autowired
+	private MicroservicePaiementProxy PaiementProxy;
 	
 	/* Etape 1 Liste des produits */
 	@RequestMapping("/")
@@ -63,6 +71,36 @@ public class ClientController {
 		
 		return "Paiement";
 	}
-
 	
+	/* Etape 5 Commande d'un produit */
+	@RequestMapping("/payer-commande/{id}")
+	public String payerCommande(@PathVariable int id, Model model) {
+		
+		// Création du paiement
+		PaiementBean paiementTodo = new PaiementBean();
+		paiementTodo.setIdCommande(id);
+		// Récupération du prix du produit à payer
+		ProductBean produit = ProduitsProxy.recupererUnProduit(id);
+		double montant = produit.getPrix();
+		// Arrondis et passage en integer...
+		paiementTodo.setMontant((int)Math.round(montant));
+		// Création du numéro de carte
+		paiementTodo.setNumeroCarte(numcarte());
+		
+		// Demande de paiement
+		ResponseEntity<PaiementBean> paiement = PaiementProxy.payerUneCommande(paiementTodo);
+		Boolean paiementAccepte = false;
+		if(paiement.getStatusCode() == HttpStatus.CREATED) {
+			paiementAccepte = true;
+		}
+		model.addAttribute("paiementOk", paiementAccepte);
+		
+		return "confirmation";
+	}
+
+	//Génére une serie de 16 chiffres au hasard pour simuler vaguement une CB
+    private Long numcarte() {
+
+        return ThreadLocalRandom.current().nextLong(1000000000000000L,9000000000000000L );
+    }
 }
